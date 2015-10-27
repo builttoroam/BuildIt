c0acbe09d592b913a7960cebe1e06b449a4936cf
Nick Randolph <nick@builttoroam.com>
2015-10-27 22:27:27 +1100

    Updating version

--- src/BuildIt.General/Properties/AssemblyInfo.cs
+++ src/BuildIt.General/Properties/AssemblyInfo.cs
@@ -26,8 +26,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.1.0")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
 [assembly: InternalsVisibleTo("BuildIt.General.Tests")]
 [assembly: InternalsVisibleTo("BuildIt.General.Explorables")]
 [assembly: InternalsVisibleTo("BuildIt.General.Tests1")]
