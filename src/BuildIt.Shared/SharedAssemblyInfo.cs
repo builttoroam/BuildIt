1146fa314a79008a9b8601abff756ca21e990e34
nickrandolph <nick@builttoroam.com>
2015-12-16 11:41:42 +1100

    Updating projects

--- src/BuildIt.Shared/SharedAssemblyInfo.cs
+++ src/BuildIt.Shared/SharedAssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Reflection;
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyVersion("0.1.1.3")]
-[assembly: AssemblyFileVersion("0.1.1.3")]
+[assembly: AssemblyVersion("0.1.1.4")]
+[assembly: AssemblyFileVersion("0.1.1.4")]
