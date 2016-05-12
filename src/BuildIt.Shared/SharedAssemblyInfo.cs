fb48e76d012e89c8f6e3ba011e51ae9e56c2b36b
Nick Randolph <nick@builttoroam.com>
2016-05-13 09:53:21 +1000

    Upgrading nuget references

--- src/BuildIt.Shared/SharedAssemblyInfo.cs
+++ src/BuildIt.Shared/SharedAssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Reflection;
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyVersion("0.1.4.1")]
-[assembly: AssemblyFileVersion("0.1.4.1")]
+[assembly: AssemblyVersion("0.1.4.2")]
+[assembly: AssemblyFileVersion("0.1.4.2")]
 
 
 // Change to 0.1.3.10
