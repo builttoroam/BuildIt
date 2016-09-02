62549ccb331d6786da3015712294fe8086e17c94
Nick Randolph <nick@builttoroam.com>
2016-09-02 10:55:51 +1000

    Updated version for UWP nuget packages

--- src/BuildIt.Shared/SharedAssemblyInfo.cs
+++ src/BuildIt.Shared/SharedAssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Reflection;
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyVersion("0.1.4.7")]
-[assembly: AssemblyFileVersion("0.1.4.7")]
+[assembly: AssemblyVersion("0.1.4.8")]
+[assembly: AssemblyFileVersion("0.1.4.8")]
 
 
