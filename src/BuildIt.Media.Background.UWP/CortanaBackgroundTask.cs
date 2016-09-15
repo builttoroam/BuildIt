843e463e3cc359e45bc0f9cd87bec566de12f906
Nick Randolph <nick@builttoroam.com>
2016-09-15 15:35:45 +1000

    Fixing background task support for cortana voice command

---
+++ src/BuildIt.Media.Background.UWP/CortanaBackgroundTask.cs
@@ -0,0 +1,15 @@
+﻿using Windows.ApplicationModel.Background;
+
+namespace BuildIt.Media.Background
+{
+    public sealed class CortanaBackgroundTask :  IBackgroundTask
+    {
+        private BaseCortanaBackgroundTask BackgroundTaskImplement { get; }= new BaseCortanaBackgroundTask();
+
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            BackgroundTaskImplement.Run(taskInstance);
+        }
+    }
+}
