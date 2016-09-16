15893bb258f353f8f584bcdca3b793be8aea6805
Richard Rachowski <richard@builttoroam.com>
2016-09-16 17:48:32 +1000

    Removing references to Microsoft.NETCore.Portable.Compatibility from general and mvvmcross, adding empty MvvmCross Droid sample

---
+++ src/Samples/MvvmCross/Buildit.MvvmCross.Droid/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.OS;
+using BuildIt.MvvmCross.ViewModels;
+
+namespace Buildit.MvvmCross.Droid
+{
+    [Activity(Label = "Buildit.MvvmCross.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Something something Buildit.MvvmCross...
+            var vm = new BaseViewModel();
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
