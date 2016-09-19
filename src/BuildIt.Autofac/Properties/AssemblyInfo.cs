315b3ce1a64a4ca5900339be521b316f2e9a2653
Richard Rachowski <richard@builttoroam.com>
2016-09-19 15:02:31 +1000

    Adding Buildit.Autofac to replace Autofac.extras.commonservicelocator

---
+++ src/BuildIt.Autofac/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BuildIt.Autofac")]
+[assembly: AssemblyDescription("Support for Autofac using BuildIt.General ServiceLocator")]
+[assembly: AssemblyProduct("BuildIt.Autofac")]
+[assembly: AssemblyConfiguration("")]
+
