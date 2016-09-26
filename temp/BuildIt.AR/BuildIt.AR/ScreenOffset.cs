1c9eba7e537fb254ccb1b71716202e7e86583b8b
MichaelBTR <michael@builttoroam.com>
2016-09-26 17:17:25 +1000

    initial commit for AR library

---
+++ temp/BuildIt.AR/BuildIt.AR/ScreenOffset.cs
@@ -0,0 +1,11 @@
+﻿namespace BuildIt.AR
+{
+    public class ScreenOffset
+    {
+        public double TranslateX { get; set; }
+        public double TranslateY { get; set; }
+        public double Scale { get; set; }
+
+        public bool IsEmpty => Equals(default(ScreenOffset));
+    }
+}
