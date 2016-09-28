60c7882354afe2db4a4c2affa3c064956e849e86
BtrJay <jay@builttoroam.com>
2016-09-28 11:48:23 +1000

    Added UWP implemntation project

---
+++ temp/BuildIt.Config/Client.Universal.impl/UWPVersdionService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Windows.ApplicationModel;
+using BuildIt.Config.Core.Standard.Services.Interfaces;
+
+namespace Client.Universal.impl
+{
+    public class UWPVersionService : IVersionService
+    {
+        public Version GetVersion()
+        {
+            return new Version(Package.Current.Id.Version.Major,
+                Package.Current.Id.Version.Minor,
+                Package.Current.Id.Version.Build,
+                Package.Current.Id.Version.Revision);
+        }
+    }
+}
