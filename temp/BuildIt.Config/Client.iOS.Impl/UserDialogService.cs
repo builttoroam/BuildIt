27554fa55e7387af6ac3b26e42732afe93d6b3a1
BtrJay <jay@builttoroam.com>
2016-09-29 11:44:20 +1000

    Only Ok button displaying out for iOS alerts

--- temp/BuildIt.Config/Client.iOS.Impl/UserDialogService.cs
+++ temp/BuildIt.Config/Client.iOS.Impl/UserDialogService.cs
@@ -14,9 +14,8 @@ namespace Client.iOS.Impl
             var tcs = new TaskCompletionSource<bool>();
             UIApplication.SharedApplication.InvokeOnMainThread(() =>
             {
-                var alert = new UIAlertView(title, message, null, "",
-                    NSBundle.MainBundle.LocalizedString("OK", "OK"));
-                alert.Clicked += (sender, buttonArgs) => tcs.SetResult(buttonArgs.ButtonIndex != alert.CancelButtonIndex);
+                var alert = new UIAlertView(title, message, null, NSBundle.MainBundle.LocalizedString("OK", "OK"));
+                alert.Clicked += (sender, buttonArgs) => tcs.SetResult(buttonArgs.ButtonIndex == alert.CancelButtonIndex);
                 alert.Show();
             });
 
