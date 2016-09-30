38a29d8cda51e253166453795e5b5a74cdcc0d15
MikolajKieres <MikolajKieres@DESKTOP-R01SJDH>
2016-09-30 15:54:46 +1000

    Fixing not building Impl.Common project

--- temp/BuildIt.Config/BuildIt.Config.Impl.Common/Properties/AssemblyInfo.cs
+++ temp/BuildIt.Config/BuildIt.Config.Impl.Common/Properties/AssemblyInfo.cs
@@ -1,11 +1,10 @@
-﻿using System.Resources;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Reflection;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
 [assembly: AssemblyTitle("BuildIt.Config.Impl.Common")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
