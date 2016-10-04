5d8ade2d26a6f79e6fcd56393a75e280d54575ea
MichaelBTR <michael@builttoroam.com>
2016-10-04 17:26:31 +1100

    complete android sample

--- temp/BuildIt.AR/BuildItArSample.iOS/ViewController.cs
+++ temp/BuildIt.AR/BuildItArSample.iOS/ViewController.cs
@@ -9,17 +9,5 @@ namespace BuildItArSample.iOS
         public ViewController(IntPtr handle) : base(handle)
         {
         }
-
-        public override void ViewDidLoad()
-        {
-            base.ViewDidLoad();
-            // Perform any additional setup after loading the view, typically from a nib.
-        }
-
-        public override void DidReceiveMemoryWarning()
-        {
-            base.DidReceiveMemoryWarning();
-            // Release any cached data, images, etc that aren't in use.
-        }
     }
 }
