0b97d78b10b8e399ec2dd11c19d09de5e3faed96
BtrJay <jay@builttoroam.com>
2016-10-04 17:14:29 +1100

    Added sample projects (iOS, UWP, Client.Core)

---
+++ src/BuildIt.Config/Samples/Client.Core/Models/State.cs
@@ -0,0 +1,9 @@
+﻿namespace Client.Core.Models
+{
+    public class State
+    {
+        public string FullName { get; set; }
+        public string ShortCode { get; set; }
+        public int StateId { get; set; }
+    }
+}
---
+++ src/BuildIt.Config/Samples/Client.Core/ViewModels/MainViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using BuildIt.Config.Core.Extensions;
+using BuildIt.Config.Core.Models;
+using BuildIt.Config.Core.Services;
+using BuildIt.Config.Core.Services.Interfaces;
+using Client.Core.Models;
+using MvvmCross.Core.ViewModels;
+
+namespace Client.Core.ViewModels
+{
+    public class MainViewModel : MvxViewModel
+    {
+        private readonly IAppConfigurationService appConfigurationService;
+
+        private ICommand getAppConfigCommand;
+        public ICommand GetAppConfigCommand => getAppConfigCommand ?? (getAppConfigCommand = new MvxCommand(async () => await GetAppConfig()));
+
+        private ObservableCollection<AppConfigurationValue> configValues = new ObservableCollection<AppConfigurationValue>();
+        public ObservableCollection<AppConfigurationValue> ConfigValues
+        {
+            get { return configValues; }
+            set
+            {
+                configValues = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public MainViewModel(IAppConfigurationService appConfigurationService)
+        {
+            this.appConfigurationService = appConfigurationService;
+        }
+
+        public override async void Start()
+        {
+            base.Start();
+
+            MapConfig();
+            await GetAppConfig();
+        }
+
+        private void MapConfig()
+        {
+            appConfigurationService.Mapper.Map("App_VersionInfo_CurrentAppVersion")
+                                          .IsRequired();            
+
+            appConfigurationService.Mapper.Map("App_States")
+                                          .IsJson();
+        }
+
+        public async Task GetAppConfig()
+        {
+            ConfigValues.Clear();
+
+            var config = await appConfigurationService.LoadAppConfig(false);
+            if (config == null) return;
+
+            foreach (var value in config.Values)
+            {
+                ConfigValues.Add(value);
+            }
+
+            await appConfigurationService.CheckMinimumVersion();
+            //var states = config?["App_States"]?.GetValue<IEnumerable<State>>();
+        }
+    }
+}
