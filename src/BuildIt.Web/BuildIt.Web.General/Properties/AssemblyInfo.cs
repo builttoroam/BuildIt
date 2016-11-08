7cbb6b8b38b3acb6682e8991f3da0f72f1f934df
Mikolaj Kieres <mikolaj@builttoroam.com>
2016-11-08 11:52:59 +1100

    Changing Web.Core project name to Web.General

--- src/BuildIt.Web/BuildIt.Web.General/Properties/AssemblyInfo.cs
+++ src/BuildIt.Web/BuildIt.Web.General/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BuildIt.Web.Core")]
+[assembly: AssemblyTitle("BuildIt.Web.General")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BuildIt.Web.Core")]
+[assembly: AssemblyProduct("BuildIt.Web.General")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
