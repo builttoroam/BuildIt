c842307d3816c57ba9ca60efad90d49ffdca9f7b
Mikolaj Kieres <mikolaj@builttoroam.com>
2016-11-21 09:42:15 +1100

    Adding environment variables extension

---
+++ src/BuildIt.Web/BuildIt.Web/Utitlites/StringExtensions.cs
@@ -0,0 +1,29 @@
+﻿#if NET45
+
+using System;
+using System.Configuration;
+
+namespace BuildIt.Web.Utitlites
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="settingsKey"></param>
+        /// <returns></returns>
+        public static string EnvironmentValue(this string settingsKey)
+        {
+            var value = Environment.GetEnvironmentVariable(settingsKey);
+
+            if (!string.IsNullOrWhiteSpace(value)) return value;
+            var environmentValue = ConfigurationManager.AppSettings[settingsKey];
+            return environmentValue;
+        }
+    }
+}
+
+#endif
