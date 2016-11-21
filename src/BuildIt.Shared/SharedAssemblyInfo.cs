1c3f483d76f2c546e6f64ca8585bdf37d898279c
MichaelBTR <michael@builttoroam.com>
2016-11-21 14:52:26 +1100

    change version number in shared assembly file to 1.0.0

--- src/BuildIt.Shared/SharedAssemblyInfo.cs
+++ src/BuildIt.Shared/SharedAssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Reflection;
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyVersion("0.1.5.6")]
-[assembly: AssemblyFileVersion("0.1.5.6")]
-[assembly: AssemblyInformationalVersion("0.1.5.6")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
 
 
