96530c3b15d983e7f427fb19f84e55846e50b88b
MichaelBui <michael@builttoroam.com>
2016-11-29 17:42:39 +1100

    add web full framework sample

---
+++ src/BuildIt.Config/Samples/BuildItConfigSample/BuildItConfigSample/BuildItConfigSample.WebFullFramework/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BuildItConfigSample.WebFullFramework.MvcApplication" Language="C#" %>
---
+++ src/BuildIt.Config/Samples/BuildItConfigSample/BuildItConfigSample/BuildItConfigSample.WebFullFramework/Global.asax.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using BuildIt.Config.Core.Api.Utilities;
+using BuildIt.Config.Core.Models;
+
+namespace BuildItConfigSample.WebFullFramework
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteTable.Routes.Add(new AppConfigurationRoute(new AppConfigurationRoutingModel() { Prefix = "test1", Controller = "configuration" }));
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
