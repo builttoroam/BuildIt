71e947a1dd0fbc2f07ce5bf029f5a09b74f94731
TheMiao <chrismiaow@gmail.com>
2016-12-06 17:29:10 +1100

    Add extra folder and files generated by autorest for cognitive services swagger.

--- src/BuildIt.CognitiveServices/BuildIt.CognitiveServices/CognitiveServicesSwagger/AcademicSearchAPI.cs
+++ src/BuildIt.CognitiveServices/BuildIt.CognitiveServices/CognitiveServicesSwagger/AcademicSearchAPI.cs
@@ -187,7 +187,7 @@ namespace BuildIt.CognitiveServices
         /// <return>
         /// A response object containing the response body and response headers.
         /// </return>
-        public async System.Threading.Tasks.Task<Microsoft.Rest.HttpOperationResponse> InterpretWithHttpMessagesAsync(string query, bool? complete = 0, double? count = 10, double? offset = default(double?), double? timeout = default(double?), string model = "latest", string subscriptionKey = default(string), string ocpApimSubscriptionKey = default(string), System.Collections.Generic.Dictionary<string, System.Collections.Generic.List<string>> customHeaders = null, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken))
+        public async System.Threading.Tasks.Task<Microsoft.Rest.HttpOperationResponse> InterpretWithHttpMessagesAsync(string query, bool? complete = false, double? count = 10, double? offset = default(double?), double? timeout = default(double?), string model = "latest", string subscriptionKey = default(string), string ocpApimSubscriptionKey = default(string), System.Collections.Generic.Dictionary<string, System.Collections.Generic.List<string>> customHeaders = null, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken))
         {
             if (query == null)
             {
