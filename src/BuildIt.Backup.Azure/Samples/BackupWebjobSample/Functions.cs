2d56a4d3b98169ac6c1a4e7fa237c11af26b44b4
duckwaffle <richard@builttoroam.com>
2017-03-17 14:38:24 +1100

    Refactoring strings and notifiers in sample

--- src/BuildIt.Backup.Azure/Samples/BackupWebjobSample/Functions.cs
+++ src/BuildIt.Backup.Azure/Samples/BackupWebjobSample/Functions.cs
@@ -17,21 +17,46 @@ namespace BackupWebjobSample
 {
     public class Functions
     {
+        private const string BackupTargetConnectionStringAppSetting = "BackupTargetConnectionString";
+        private const string BlobBackupSourceConnectionStringAppSetting = "BackupSourceConnectionString";
+        private const string BlobBackupSourceContainerAppSetting = "BackupSourceContainer";
+        private const string DbBackupServerNameAppSetting = "DbBackupServerName";
+        private const string DbBackupDatabaseAppSetting = "DbBackupDatabaase";
+        private const string DbBackupUsernameAppSetting = "DbBackupUsername";
+        private const string DbBackupPasswordAppSetting = "DbBackupPassword";
+        private const string DbBackupTargetContainerAppSetting = "DbBackupTargetContainer";
+        private const string DbBackupRegionAppSetting = "DbBackupRegion";
+        private const string NumberOfBackupsToRetainAppSetting = "NumberOfBackupsToRetain";
+
+        private const string BlobBackupQueueName = "blobbackuptest";
+        private const string DbBackupQueueName = "dbbackuptest";
+
+        private readonly IBlobBackupNotifier blobBackupNotifier;
+        private readonly IDbBackupNotifier dbBackupNotifier;
+
+        public Functions()
+        {
+            blobBackupNotifier = new BlobBackupQueueNotifier(
+                ConfigurationManager.ConnectionStrings["AzureWebJobsStorage"].ConnectionString,
+                BlobBackupQueueName,
+                TimeSpan.FromSeconds(30));
+
+            dbBackupNotifier = new DbBackupQueueNotifier(
+                ConfigurationManager.ConnectionStrings["AzureWebJobsStorage"].ConnectionString,
+                DbBackupQueueName,
+                TimeSpan.FromSeconds(30));
+        }
+
         // Note, for the purposes of this demo, this function is called on webjob startup, and is only called once
         // The function that initiates the backup process should always be a singleton, either explicitly, or via a singleton type trigger
         [NoAutomaticTrigger]
         [Singleton]
         public async Task InitiateBlobStorageBackup(TraceWriter log)
         {
-            var sourceStorageAccountConnectionString = ConfigurationManager.AppSettings["BackupSourceConnectionString"];
-            var targetStorageAccountConnectionString = ConfigurationManager.AppSettings["BackupTargetConnectionString"];
-            var sourceContainerName = ConfigurationManager.AppSettings["BackupSourceContainer"];
+            var sourceStorageAccountConnectionString = ConfigurationManager.AppSettings[BlobBackupSourceConnectionStringAppSetting];
+            var targetStorageAccountConnectionString = ConfigurationManager.AppSettings[BackupTargetConnectionStringAppSetting];
+            var sourceContainerName = ConfigurationManager.AppSettings[BlobBackupSourceContainerAppSetting];
             var targetContainerName = sourceContainerName + DateTime.UtcNow.ToString("yyyyMMdd-HHmmss");
-
-            var queueNotifier = new BlobBackupQueueNotifier(
-                ConfigurationManager.ConnectionStrings["AzureWebJobsStorage"].ConnectionString,
-                "blobbackuptest",
-                TimeSpan.FromSeconds(30));
 
             log.Info($"Initiating backup of container: {sourceContainerName}");
 
@@ -40,22 +65,22 @@ namespace BackupWebjobSample
                 targetStorageAccountConnectionString,
                 sourceContainerName,
                 targetContainerName,
-                queueNotifier,
+                blobBackupNotifier,
                 log);
         }
 
         // This function will get triggered/executed when a new message is written 
         // on an Azure Queue called backuptest.
         public async Task ProcessBlobBackupQueueMessage(
-            [QueueTrigger("blobbackuptest")] BlobBackupOperationNotification message,
+            [QueueTrigger(BlobBackupQueueName)] BlobBackupOperationNotification message,
             TraceWriter log)
         {
             log.Info($"Processing incomming blob container backup queue message: {JsonConvert.SerializeObject(message)}");
 
-            var sourceStorageAccountConnectionString = ConfigurationManager.AppSettings["BackupSourceConnectionString"];
-            var targetStorageAccountConnectionString = ConfigurationManager.AppSettings["BackupTargetConnectionString"];
-            var sourceContainerName = ConfigurationManager.AppSettings["BackupSourceContainer"];
-            int.TryParse(ConfigurationManager.AppSettings["NumberOfBackupsToRetain"], out var backupRetentionPolicy);
+            var sourceStorageAccountConnectionString = ConfigurationManager.AppSettings[BlobBackupSourceConnectionStringAppSetting];
+            var targetStorageAccountConnectionString = ConfigurationManager.AppSettings[BackupTargetConnectionStringAppSetting];
+            var sourceContainerName = ConfigurationManager.AppSettings[BlobBackupSourceContainerAppSetting];
+            int.TryParse(ConfigurationManager.AppSettings[NumberOfBackupsToRetainAppSetting], out var backupRetentionPolicy);
 
             if ((!sourceStorageAccountConnectionString.Contains(message.SourceStorageAccountName) ||
                 !targetStorageAccountConnectionString.Contains(message.TargetStorageAccountName) ||
@@ -63,11 +88,6 @@ namespace BackupWebjobSample
             {
                 throw new Exception("This message is not intended for this function");
             }
-
-            var queueNotifier = new BlobBackupQueueNotifier(
-                ConfigurationManager.ConnectionStrings["AzureWebJobsStorage"].ConnectionString,
-                "backuptest",
-                TimeSpan.FromSeconds(30));
 
             switch (message.OperationType)
             {
@@ -78,7 +98,7 @@ namespace BackupWebjobSample
                         message.TargetContainerName,
                         message.SourceStorageAccountName,
                         sourceContainerName,
-                        queueNotifier,
+                        blobBackupNotifier,
                         log);
                     break;
                 case BackupOperationType.InProgress:
@@ -88,7 +108,7 @@ namespace BackupWebjobSample
                         message.TargetContainerName,
                         message.SourceStorageAccountName,
                         sourceContainerName,
-                        queueNotifier,
+                        blobBackupNotifier,
                         log);
                     break;
                 case BackupOperationType.Complete:
@@ -99,7 +119,7 @@ namespace BackupWebjobSample
                         message.TargetContainerName,
                         sourceContainerName,
                         backupRetentionPolicy,
-                        queueNotifier,
+                        blobBackupNotifier,
                         log);
                     break;
                 case BackupOperationType.Error:
@@ -117,37 +137,32 @@ namespace BackupWebjobSample
         [Singleton]
         public async Task InitiateDatabaseStorageBackup(TraceWriter log)
         {
-            var serverName = ConfigurationManager.AppSettings["DbBackupServerName"];
-            var dbName = ConfigurationManager.AppSettings["DbBackupDatabase"];
-            var username = ConfigurationManager.AppSettings["DbBackupUsername"];
-            var password = ConfigurationManager.AppSettings["DbBackupPassword"];
-            var targetStorageAccountConnectionString = ConfigurationManager.AppSettings["BackupTargetConnectionString"];
-            var targetContainer = ConfigurationManager.AppSettings["DbBackupTargetContainer"];
-            Enum.TryParse(ConfigurationManager.AppSettings["DbBackupRegion"], out AzureDbLocation dbRegion);
-
-            var queueNotifier = new DbBackupQueueNotifier(
-                ConfigurationManager.ConnectionStrings["AzureWebJobsStorage"].ConnectionString,
-                "dbbackuptest",
-                TimeSpan.FromSeconds(30));
+            var serverName = ConfigurationManager.AppSettings[DbBackupServerNameAppSetting];
+            var dbName = ConfigurationManager.AppSettings[DbBackupDatabaseAppSetting];
+            var username = ConfigurationManager.AppSettings[DbBackupUsernameAppSetting];
+            var password = ConfigurationManager.AppSettings[DbBackupPasswordAppSetting];
+            var targetStorageAccountConnectionString = ConfigurationManager.AppSettings[BackupTargetConnectionStringAppSetting];
+            var targetContainer = ConfigurationManager.AppSettings[DbBackupTargetContainerAppSetting];
+            Enum.TryParse<AzureDbLocation>(ConfigurationManager.AppSettings[DbBackupRegionAppSetting], out var dbRegion);
 
             await BackupSqlDb.InitiateDbBackup(serverName, dbName, username, password, dbRegion,
-                targetStorageAccountConnectionString, targetContainer, queueNotifier, log);
+                targetStorageAccountConnectionString, targetContainer, dbBackupNotifier, log);
         }
 
         public async Task ProcessDbBackupQueueMessage(
-            [QueueTrigger("dbbackuptest")] DbBackupOperationNotification message,
+            [QueueTrigger(DbBackupQueueName)] DbBackupOperationNotification message,
             TraceWriter log)
         {
             log.Info($"Processing incomming database backup queue message: {JsonConvert.SerializeObject(message)}");
 
-            var serverName = ConfigurationManager.AppSettings["DbBackupServerName"];
-            var dbName = ConfigurationManager.AppSettings["DbBackupDatabase"];
-            var username = ConfigurationManager.AppSettings["DbBackupUsername"];
-            var password = ConfigurationManager.AppSettings["DbBackupPassword"];
-            Enum.TryParse<AzureDbLocation>(ConfigurationManager.AppSettings["DbBackupRegion"], out var dbRegion);
-            int.TryParse(ConfigurationManager.AppSettings["NumberOfBackupsToRetain"], out var backupRetentionPolicy);
-            var targetStorageAccountConnectionString = ConfigurationManager.AppSettings["BackupTargetConnectionString"];
-            var targetContainer = ConfigurationManager.AppSettings["DbBackupTargetContainer"];
+            var serverName = ConfigurationManager.AppSettings[DbBackupServerNameAppSetting];
+            var dbName = ConfigurationManager.AppSettings[DbBackupDatabaseAppSetting];
+            var username = ConfigurationManager.AppSettings[DbBackupUsernameAppSetting];
+            var password = ConfigurationManager.AppSettings[DbBackupPasswordAppSetting];
+            Enum.TryParse<AzureDbLocation>(ConfigurationManager.AppSettings[DbBackupRegionAppSetting], out var dbRegion);
+            int.TryParse(ConfigurationManager.AppSettings[NumberOfBackupsToRetainAppSetting], out var backupRetentionPolicy);
+            var targetStorageAccountConnectionString = ConfigurationManager.AppSettings[BackupTargetConnectionStringAppSetting];
+            var targetContainer = ConfigurationManager.AppSettings[DbBackupTargetContainerAppSetting];
 
             if ((serverName != message.DbServer ||
                  dbName != message.DbName) &&
@@ -155,11 +170,6 @@ namespace BackupWebjobSample
             {
                 throw new Exception("This message is not intended for this function");
             }
-
-            var queueNotifier = new DbBackupQueueNotifier(
-                ConfigurationManager.ConnectionStrings["AzureWebJobsStorage"].ConnectionString,
-                "dbbackuptest",
-                TimeSpan.FromSeconds(30));
 
             switch (message.OperationType)
             {
@@ -173,7 +183,7 @@ namespace BackupWebjobSample
                         dbRegion,
                         message.BackupBlobName,
                         message.OperationId,
-                        queueNotifier,
+                        dbBackupNotifier,
                         log);
                     break;
                 case BackupOperationType.InProgress:
@@ -186,7 +196,7 @@ namespace BackupWebjobSample
                         dbRegion,
                         message.BackupBlobName,
                         message.OperationId,
-                        queueNotifier,
+                        dbBackupNotifier,
                         log);
                     break;
                 case BackupOperationType.Complete:
@@ -198,7 +208,7 @@ namespace BackupWebjobSample
                         targetContainer,
                         message.BackupBlobName,
                         backupRetentionPolicy,
-                        queueNotifier,
+                        dbBackupNotifier,
                         log);
                     break;
                 case BackupOperationType.Error:
