2882bec0864aaf6ec9e6a3e802e14f8b40a050dc
duckwaffle <richard@builttoroam.com>
2017-03-17 14:17:54 +1100

    Adding null notifier for db backup

---
+++ src/BuildIt.Backup.Azure/BuildIt.Backup.Azure/Operations/DbBackupNullNotifier.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildIt.Backup.Azure.Operations
+{
+    public class DbBackupNullNotifier : IDbBackupNotifier
+    {
+        public Task Init()
+        {
+            return Task.Delay(0);
+        }
+
+        public Task NotifyBackupInitiated(string dbServer, string dbName, string backupBlobName, Guid operationId)
+        {
+            return Task.Delay(0);
+        }
+
+        public Task NotifyBackupProgress(string dbServer, string dbName, string backupBlobName, Guid operationId, bool backupInProgress)
+        {
+            return Task.Delay(0);
+        }
+
+        public Task NotifyBackupError(string dbServer, string dbName, string backupBlobName, Guid operationId, string errorMessage)
+        {
+            return Task.Delay(0);
+        }
+    }
+}
