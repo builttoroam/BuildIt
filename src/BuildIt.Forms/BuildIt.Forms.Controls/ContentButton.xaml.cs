40f0196997b4f168c643ccd68e2cc539628ca43e
Nick Randolph <nick@builttoroam.com>
2017-06-26 08:55:08 +1000

    Adding command support to content button

--- src/BuildIt.Forms/BuildIt.Forms.Controls/ContentButton.xaml.cs
+++ src/BuildIt.Forms/BuildIt.Forms.Controls/ContentButton.xaml.cs
@@ -6,13 +6,78 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using BuildIt.Forms.Core;
+using System.Windows.Input;
+using Xamarin.Forms.Internals;
+using System.Runtime.CompilerServices;
 
 namespace BuildIt.Forms.Controls
 {
 	public partial class ContentButton : ContentView
 	{
         public event EventHandler Pressed;
-      
+
+        public static readonly BindableProperty CommandProperty = 
+            BindableProperty.Create("Command", typeof(ICommand), typeof(ContentButton), null, 
+                BindingMode.OneWay, null, new BindableProperty.BindingPropertyChangedDelegate(CommandChanged), null, null, null);
+
+        private static void CommandParameterChanged(BindableObject bindable, object oldvalue, object newvalue)
+        {
+            ((ContentButton)bindable).CommandCanExecuteChanged(bindable, EventArgs.Empty);
+        }
+
+        public static readonly BindableProperty CommandParameterProperty = 
+            BindableProperty.Create("CommandParameter", typeof(object), typeof(Button), null, 
+                BindingMode.OneWay, null, new BindableProperty.BindingPropertyChangedDelegate(CommandParameterChanged), null, null, null);
+
+        private static void CommandChanged(BindableObject bo, object o, object n)
+        {
+            ((ContentButton)bo).OnCommandChanged();
+        }
+
+        private void OnCommandChanged()
+        {
+            if (this.Command != null)
+            {
+                this.Command.CanExecuteChanged += new EventHandler(this.CommandCanExecuteChanged);
+                this.CommandCanExecuteChanged(this, EventArgs.Empty);
+                return;
+            }
+            this.IsEnabledCore = true;
+        }
+
+
+        private bool IsEnabledCore
+        {
+            set
+            {
+                base.SetValueCore(VisualElement.IsEnabledProperty, value, SetValueFlags.None);
+            }
+        }
+
+
+        public ICommand Command
+        {
+            get
+            {
+                return (ICommand)base.GetValue(ContentButton.CommandProperty);
+            }
+            set
+            {
+                base.SetValue(ContentButton.CommandProperty, value);
+            }
+        }
+
+        public object CommandParameter
+        {
+            get
+            {
+                return base.GetValue(ContentButton.CommandParameterProperty);
+            }
+            set
+            {
+                base.SetValue(ContentButton.CommandParameterProperty, value);
+            }
+        }
 
         public ContentButton ()
 		{
@@ -64,12 +129,21 @@ namespace BuildIt.Forms.Controls
                     IsPressed = false;
                     if(IsEntered || !EnterRequired)
                     {
-                        Pressed.SafeRaise(this);
+                        SendPressed();
                     }
                     break;
             }
             UpdateVisualState();
         }
+
+
+        public void SendPressed()
+        {
+            Command?.Execute(CommandParameter) ;
+            Pressed.SafeRaise(this);
+
+        }
+
 
         private void UpdateVisualState()
         {
@@ -95,5 +169,39 @@ namespace BuildIt.Forms.Controls
             }
         }
 
+        protected override void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            base.OnPropertyChanged(propertyName);
+            if (propertyName == ContentButton.CommandProperty.PropertyName)
+            {
+                ICommand cmd = this.Command;
+                if (cmd != null)
+                {
+                    cmd.CanExecuteChanged += new EventHandler(this.CommandCanExecuteChanged);
+                }
+            }
+        }
+
+        protected override void OnPropertyChanging([CallerMemberName] string propertyName = null)
+        {
+            if (propertyName == ContentButton.CommandProperty.PropertyName)
+            {
+                ICommand cmd = this.Command;
+                if (cmd != null)
+                {
+                    cmd.CanExecuteChanged -= new EventHandler(this.CommandCanExecuteChanged);
+                }
+            }
+            base.OnPropertyChanging(propertyName);
+        }
+
+        private void CommandCanExecuteChanged(object sender, EventArgs eventArgs)
+        {
+            ICommand cmd = this.Command;
+            if (cmd != null)
+            {
+                this.IsEnabledCore = cmd.CanExecute(this.CommandParameter);
+            }
+        }
     }
 }
--- src/BuildIt.Forms/BuildIt.forms.Sample.Core/ViewModels/MainViewModel.cs
+++ src/BuildIt.Forms/BuildIt.forms.Sample.Core/ViewModels/MainViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using BuildIt.States;
+using System.Windows.Input;
 
 namespace BuildIt.forms.Sample.Core.ViewModels
 {
@@ -15,6 +16,8 @@ namespace BuildIt.forms.Sample.Core.ViewModels
 
     public class MainViewModel:IHasStates
     {
+        private ICommand pressedCommand;
+        public ICommand PressedCommand => pressedCommand ?? (pressedCommand = new Command(SwitchStates));
 
         public IStateManager StateManager { get; } = new StateManager();
 
@@ -30,6 +33,97 @@ namespace BuildIt.forms.Sample.Core.ViewModels
         {
          visible = !visible;
             StateManager.GoToState(visible ? SampleStates.Show : SampleStates.Hide);
+        }
+    }
+
+
+
+
+    public class Command : ICommand
+    {
+        private readonly Func<object, bool> _canExecute;
+
+        private readonly Action<object> _execute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public Command(Action<object> execute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+            this._execute = execute;
+        }
+
+        public Command(Action execute) : this(delegate (object o)
+        {
+            execute();
+        })
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+        }
+
+        public Command(Action<object> execute, Func<object, bool> canExecute) : this(execute)
+        {
+            if (canExecute == null)
+            {
+                throw new ArgumentNullException("canExecute");
+            }
+            this._canExecute = canExecute;
+        }
+
+        public Command(Action execute, Func<bool> canExecute) : this(delegate (object o)
+        {
+            execute();
+        }, (object o) => canExecute())
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+            if (canExecute == null)
+            {
+                throw new ArgumentNullException("canExecute");
+            }
+        }
+
+        /// <param name="parameter">An <see cref="T:System.Object" /> used as parameter to determine if the Command can be executed.</param>
+        /// <summary>Returns a <see cref="T:System.Boolean" /> indicating if the Command can be exectued with the given parameter.</summary>
+        /// <returns>
+        ///     <see langword="true" /> if the Command can be executed, <see langword="false" /> otherwise.</returns>
+        /// <remarks>
+        ///     <para>If no canExecute parameter was passed to the Command constructor, this method always returns <see langword="true" />.</para>
+        ///     <para>If the Command was created with non-generic execute parameter, the parameter of this method is ignored.</para>
+        /// </remarks>
+        public bool CanExecute(object parameter)
+        {
+            return this._canExecute == null || this._canExecute(parameter);
+        }
+
+        /// <param name="parameter">An <see cref="T:System.Object" /> used as parameter for the execute Action.</param>
+        /// <summary>Invokes the execute Action</summary>
+        /// <remarks>
+        ///     <para>If the Command was created with non-generic execute parameter, the parameter of this method is ignored.</para>
+        /// </remarks>
+        public void Execute(object parameter)
+        {
+            this._execute(parameter);
+        }
+
+        /// <summary>Send a <see cref="E:System.Windows.Input.ICommand.CanExecuteChanged" /></summary>
+        /// <remarks />
+        public void ChangeCanExecute()
+        {
+            EventHandler expr_06 = this.CanExecuteChanged;
+            if (expr_06 == null)
+            {
+                return;
+            }
+            expr_06(this, EventArgs.Empty);
         }
     }
 }
