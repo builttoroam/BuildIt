ea81e7128a6930239f91ab1b107b7b26af3bc18f
Nick Randolph <nick@builttoroam.com>
2017-07-08 16:50:11 +1000

    Tidying up assembly information and adding full debug info to release builds

--- src/BuildIt.General/BuildIt.General/Properties/AssemblyInfo.cs
+++ src/BuildIt.General/BuildIt.General/Properties/AssemblyInfo.cs
@@ -4,9 +4,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("BuildIt.General")]
 [assembly: AssemblyDescription("This library contains a number of helper methods and functions that make building applications on a variety of platforms easier")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("BuildIt.General")]
+[assembly: AssemblyProduct("BuildIt Libraries")]
 
 [assembly: InternalsVisibleTo("BuildIt.General.Tests")]
 [assembly: InternalsVisibleTo("BuildIt.General.Explorables")]
-[assembly: InternalsVisibleTo("BuildIt.General.Tests1")]
-[assembly: InternalsVisibleTo("BuildIt.General.Tests2")]
