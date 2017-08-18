1ccb55e8af444eae62bab6c95311fb74436a38f7
Nick Randolph <nick@builttoroam.com>
2017-08-18 17:22:42 +1000

    Upgrading to latest xamarin forms preview

---
+++ src/BuildIt.Forms/BuildIt.Forms.Core/Animations/StoryboardTriggerAction.cs
@@ -0,0 +1,34 @@
+using System.Threading;
+using Xamarin.Forms;
+
+namespace BuildIt.Forms.Animations
+{
+    /// <summary>
+    /// Trigger action to run a storyboard
+    /// </summary>
+    public class StoryboardTriggerAction : TriggerAction<Button>
+    {
+        /// <summary>
+        /// Gets or sets animations to be run prior to a state change
+        /// </summary>
+        public Storyboard Storyboard
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Application developers override this method to provide the action that is performed when the trigger condition is met.
+        /// </summary>
+        /// <param name="entry">The button to invoke the action on</param>
+        protected override async void Invoke(Button entry)
+        {
+            if (Storyboard == null)
+            {
+                return;
+            }
+
+            await Storyboard.Animate(CancellationToken.None);
+        }
+    }
+}
