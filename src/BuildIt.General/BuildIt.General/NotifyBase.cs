04373058e75df91b51da42e5009313a350eabc7b
Nick Randolph <nick@builttoroam.com>
2017-09-12 23:25:56 +1000

    Adding SetProperty to NotifyBase LogService can be manually set

--- src/BuildIt.General/BuildIt.General/NotifyBase.cs
+++ src/BuildIt.General/BuildIt.General/NotifyBase.cs
@@ -36,5 +36,26 @@ namespace BuildIt
             var name = (selector.Body as MemberExpression)?.Member.Name;
             handler?.Invoke(this, new PropertyChangedEventArgs(name));
         }
+
+        /// <summary>
+        /// Sets a backing field and raises PropertyChanged if required
+        /// </summary>
+        /// <typeparam name="T">The type of the field to update</typeparam>
+        /// <param name="storage">The reference to the field</param>
+        /// <param name="value">The value to update the field with</param>
+        /// <param name="propertyName">The name of the property</param>
+        /// <returns>Indicates if the property has changed</returns>
+        protected virtual bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return false;
+            }
+
+            storage = value;
+            // ReSharper disable once ExplicitCallerInfoArgument - don't want "SetProperty" to be passed in
+            OnPropertyChanged(propertyName);
+            return true;
+        }
     }
 }
