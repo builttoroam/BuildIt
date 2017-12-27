ccab9098c5d28dff25a19b2a627f7bd9d013bea9
Nick Randolph <nick@builttoroam.com>
2017-12-28 01:49:32 +1100

    Fixing up assembly information

--- src/BuildIt.MvvmCross/BuildIt.MvvmCross.Forms.Droid/Properties/AssemblyInfo.cs
+++ src/BuildIt.MvvmCross/BuildIt.MvvmCross.Forms.Droid/Properties/AssemblyInfo.cs
@@ -8,23 +8,4 @@ using Android.App;
 // associated with an assembly.
 [assembly: AssemblyTitle("BuildIt.MvvmCross.Forms.Droid")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BuildIt.MvvmCross.Forms.Droid")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
--- src/BuildIt.MvvmCross/BuildIt.MvvmCross.Forms.UWP/Properties/AssemblyInfo.cs
+++ src/BuildIt.MvvmCross/BuildIt.MvvmCross.Forms.UWP/Properties/AssemblyInfo.cs
@@ -8,22 +8,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("BuildIt.MvvmCross.Forms.UWP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BuildIt.MvvmCross.Forms.UWP")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: ComVisible(false)]
