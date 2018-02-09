3fff35c2278b2663831b8edf6ed6a8d88b2ac597
Nick Randolph <nick@builttoroam.com>
2018-02-09 18:12:20 +1100

    Adding multi-targetting support

--- src/BuildIt.States/BuildIt.States.UWP/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("BuildIt.States.UWP")]
-[assembly: AssemblyDescription("UWP binding for State Manager")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("BuildIt.States.UWP")]
