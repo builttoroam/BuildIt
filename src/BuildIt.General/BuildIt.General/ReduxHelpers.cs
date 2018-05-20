e759df7107c95fb83c777eb888a29dbdb283c5b0
Nick Randolph <nick@builttoroam.com>
2018-05-20 12:16:35 +1000

    Adding redux methods Adjusting fluent method signatures Adding test cases (fluent cases dropped as intellitest can't handle complex generics)

--- src/BuildIt.General/BuildIt.General/ReduxHelpers.cs
+++ src/BuildIt.General/BuildIt.General/ReduxHelpers.cs
@@ -18,18 +18,93 @@ namespace BuildIt
         public static ObservableCollection<T> DeepClone<T>(this ObservableCollection<T> source)
             where T : new()
         {
+            return source?.Map<ObservableCollection<T>, T>(item => item.DeepEntityClone());
+        }
+
+        /// <summary>
+        /// Does a deep clone of an entity
+        /// </summary>
+        /// <typeparam name="T">The type of entity to clone</typeparam>
+        /// <param name="source">The source entity</param>
+        /// <returns>The cloned entity</returns>
+        public static T DeepEntityClone<T>(this T source)
+            where T : new()
+        {
             if (source == null)
             {
-                return null;
+                return default;
             }
 
             var helper = TypeHelper.RetrieveHelperForType(typeof(T));
-            return source.Map<ObservableCollection<T>, T>(item =>
+            var newItem = new T();
+            helper.DeepUpdater(newItem, source);
+            return newItem;
+        }
+
+        /// <summary>
+        /// Clones and adds item
+        /// </summary>
+        /// <typeparam name="T">Type of item in collection</typeparam>
+        /// <param name="source">source collection</param>
+        /// <param name="itemToAdd">Item to add</param>
+        /// <returns>new collection</returns>
+        public static ObservableCollection<T> DeepAdd<T>(this ObservableCollection<T> source, T itemToAdd)
+            where T : new()
+        {
+            var clonedItem = itemToAdd.DeepEntityClone();
+            var collection = source.DeepClone().AddItem(clonedItem);
+            return collection;
+        }
+
+        /// <summary>
+        /// Clones and inserts item
+        /// </summary>
+        /// <typeparam name="T">Type of item in collection</typeparam>
+        /// <param name="source">source collection</param>
+        /// <param name="index">position to insert item</param>
+        /// <param name="itemToAdd">Item to insert</param>
+        /// <returns>new collection</returns>
+        public static ObservableCollection<T> DeepInsert<T>(this ObservableCollection<T> source, int index, T itemToAdd)
+            where T : new()
+        {
+            var clonedItem = itemToAdd.DeepEntityClone();
+            var collection = source.DeepClone().InsertItem(index, clonedItem);
+            return collection;
+        }
+
+        /// <summary>
+        /// Clones and removes item
+        /// </summary>
+        /// <typeparam name="T">Type of item in collection</typeparam>
+        /// <param name="source">source collection</param>
+        /// <param name="itemToRemove">item to remove</param>
+        /// <returns>new collection</returns>
+        public static ObservableCollection<T> DeepRemove<T>(this ObservableCollection<T> source, T itemToRemove)
+            where T : new()
+        {
+            return source?.Map<ObservableCollection<T>, T>(item =>
             {
-                var newItem = new T();
-                helper.DeepUpdater(newItem, item);
-                return newItem;
+                if (item != null && item.Equals(itemToRemove))
+                {
+                    return default;
+                }
+
+                return item.DeepEntityClone();
             });
+        }
+
+        /// <summary>
+        /// Clones and removes item
+        /// </summary>
+        /// <typeparam name="T">Type of item in collection</typeparam>
+        /// <param name="source">source collection</param>
+        /// <param name="index">position of item to remove</param>
+        /// <returns>new collection</returns>
+        public static ObservableCollection<T> DeepRemoveAt<T>(this ObservableCollection<T> source, int index)
+            where T : new()
+        {
+            var collection = source.DeepClone().RemoveItemAt<ObservableCollection<T>, T>(index);
+            return collection;
         }
 
         /// <summary>
@@ -41,7 +116,7 @@ namespace BuildIt
         /// <param name="map">The transform function</param>
         /// <returns>The generated collection</returns>
         public static TCollection Map<TCollection, TItem>(this TCollection sourceCollection, Func<TItem, TItem> map)
-            where TCollection : IList<TItem>, new()
+        where TCollection : IList<TItem>, new()
         {
             var targetCollection = new TCollection();
             foreach (var item in sourceCollection)
