bc76c2f47b1f64a2789f5b9c8c6b9ba6e9e98585
RichardRachowski <richard@builttoroam.com>
2018-07-11 15:30:50 +1000

    Initial implementation of camera preview control renderer for android

---
+++ src/BuildIt.Forms/BuildIt.Forms.Controls/Platforms/Android/CameraPreviewControlRenderer.cs
@@ -0,0 +1,141 @@
+﻿using Android.App;
+using Android.Graphics;
+using Android.Hardware;
+using Android.Views;
+using Android.Widget;
+using BuildIt.Forms.Controls;
+using BuildIt.Forms.Controls.Platforms.Android;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Context = Android.Content.Context;
+
+[assembly: ExportRenderer(typeof(CameraPreviewControl), typeof(CameraPreviewControlRenderer))]
+namespace BuildIt.Forms.Controls.Platforms.Android
+{
+    /// <summary>
+    /// Custom Renderer for <see cref="CameraPreviewControl"/>
+    /// </summary>
+    public class CameraPreviewControlRenderer : FrameRenderer, TextureView.ISurfaceTextureListener
+    {
+        private global::Android.Hardware.Camera camera;
+        private global::Android.Views.View view;
+
+        private Activity activity;
+        private CameraFacing cameraType;
+        private TextureView textureView;
+        private SurfaceTexture surfaceTexture;
+
+        private bool flashOn;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CameraPreviewControlRenderer"/> class.
+        /// </summary>
+        /// <param name="context"><see cref="Context"/></param>
+        public CameraPreviewControlRenderer(Context context)
+            : base(context)
+        {
+        }
+
+        /// <inheritdoc />
+        public void OnSurfaceTextureAvailable(SurfaceTexture surface, int width, int height)
+        {
+            camera = global::Android.Hardware.Camera.Open((int)cameraType);
+            textureView.LayoutParameters = new FrameLayout.LayoutParams(width, height);
+            surfaceTexture = surface;
+
+            camera.SetPreviewTexture(surface);
+            PrepareAndStartCamera();
+        }
+
+        /// <inheritdoc />
+        public bool OnSurfaceTextureDestroyed(SurfaceTexture surface)
+        {
+            camera.StopPreview();
+            camera.Release();
+            return true;
+        }
+
+        /// <inheritdoc />
+        public void OnSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height)
+        {
+            PrepareAndStartCamera();
+        }
+
+        /// <inheritdoc />
+        public void OnSurfaceTextureUpdated(SurfaceTexture surface)
+        {
+        }
+
+        /// <inheritdoc />
+        protected override void OnElementChanged(ElementChangedEventArgs<Frame> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Element == null)
+            {
+                return;
+            }
+
+            try
+            {
+                SetupUserInterface();
+                SetupEventHandlers();
+                AddView(view);
+            }
+            catch (Exception ex)
+            {
+                ex.LogError();
+            }
+        }
+
+        /// <inheritdoc />
+        protected override void OnLayout(bool changed, int l, int t, int r, int b)
+        {
+            base.OnLayout(changed, l, t, r, b);
+
+            var msw = MeasureSpec.MakeMeasureSpec(r - l, MeasureSpecMode.Exactly);
+            var msh = MeasureSpec.MakeMeasureSpec(b - t, MeasureSpecMode.Exactly);
+
+            view.Measure(msw, msh);
+            view.Layout(0, 0, r - l, b - t);
+        }
+
+        private void SetupUserInterface()
+        {
+            activity = Context as Activity;
+            if (activity == null)
+            {
+                return;
+            }
+
+            view = activity.LayoutInflater.Inflate(Resource.Layout.CameraLayout, this, false);
+            cameraType = CameraFacing.Back;
+
+            textureView = view.FindViewById<TextureView>(Resource.Id.textureView);
+            textureView.SurfaceTextureListener = this;
+        }
+
+        private void SetupEventHandlers()
+        {
+        }
+
+        private void PrepareAndStartCamera()
+        {
+            camera.StopPreview();
+
+            var display = activity.WindowManager.DefaultDisplay;
+            if (display.Rotation == SurfaceOrientation.Rotation0)
+            {
+                camera.SetDisplayOrientation(90);
+            }
+
+            if (display.Rotation == SurfaceOrientation.Rotation270)
+            {
+                camera.SetDisplayOrientation(180);
+            }
+
+            camera.StartPreview();
+        }
+    }
+}
