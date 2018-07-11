b29ae278cf4b8c0cb8074b4292da32852eaa45a7
RichardRachowski <richard@builttoroam.com>
2018-07-11 11:45:35 +1000

    Adding CameraPreviewControl and renderer for UWP

---
+++ src/BuildIt.Forms/BuildIt.Forms.Controls/CameraPreviewControl.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BuildIt.Forms.Controls
+{
+    /// <summary>
+    /// Represents a ContentView which can be used to display a live preview of a device's camera
+    /// </summary>
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CameraPreviewControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CameraPreviewControl"/> class.
+        /// </summary>
+        public CameraPreviewControl()
+        {
+            InitializeComponent();
+        }
+
+        ///// <summary>
+        ///// Gets the <see cref="Image"/> used for rendering the camera preview
+        ///// </summary>
+        //internal Image PreviewImage => CameraPreviewImage;
+    }
+}
