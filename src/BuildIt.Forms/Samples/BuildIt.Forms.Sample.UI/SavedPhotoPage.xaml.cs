2840cf0a553acc4d6cc54d1c2811da26b3ef7079
RichardRachowski <richard@builttoroam.com>
2018-07-13 10:27:01 +1000

    Ability to save video frames on ios

---
+++ src/BuildIt.Forms/Samples/BuildIt.Forms.Sample.UI/SavedPhotoPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms.Xaml;
+
+namespace BuildIt.Forms.Sample
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SavedPhotoPage
+    {
+        public SavedPhotoPage(string photoFilePath)
+        {
+            InitializeComponent();
+            Image.Source = photoFilePath;
+        }
+
+        private async void CloseButton_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync(true);
+        }
+    }
+}
