98a566b2aef28d0331d522a1d0160828a8795b70
MaikuB <25263378+MaikuB@users.noreply.github.com>
2018-07-27 16:29:47 +1000

    initial commit of BuildIt.ML

---
+++ src/BuildIt.ML/BuildIt.ML/Platforms/Android/Classifier.cs
@@ -0,0 +1,28 @@
+﻿using Org.Tensorflow.Contrib.Android;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuildIt.ML.Platforms.Android
+{
+    public class Classifier
+    {
+        private TensorFlowInferenceInterface inferenceInterface;
+        private List<string> labels;
+
+        public async Task InitAsync(string modelName, string labelsFileName)
+        {
+            var assets = global::Android.App.Application.Context.Assets;
+
+            // TODO: investigate if list of labels can just be passed in without reading from the file
+            using (var sr = new StreamReader(assets.Open(labelsFileName)))
+            {
+                var content = sr.ReadToEnd();
+                labels = content.Split('\n').Select(s => s.Trim()).Where(s => !string.IsNullOrEmpty(s)).ToList();
+            }
+
+            inferenceInterface = new TensorFlowInferenceInterface(assets, modelName);
+        }
+    }
+}
---
+++ src/BuildIt.ML/BuildIt.ML/Platforms/Ios/Classifier.cs
@@ -0,0 +1,19 @@
+﻿using CoreML;
+using Foundation;
+using System.Threading.Tasks;
+
+namespace BuildIt.ML.Platforms.Ios
+{
+    public class Classifier
+    {
+        private const string CoreMLModelExtension = "mlmodelc";
+        private MLModel model;
+
+        public async Task InitAsync(string modelName)
+        {
+            var assetPath = NSBundle.MainBundle.GetUrlForResource(modelName, CoreMLModelExtension);
+            model = MLModel.Create(assetPath, out NSError error);
+            // TODO: handle when there's an error
+        }
+    }
+}
