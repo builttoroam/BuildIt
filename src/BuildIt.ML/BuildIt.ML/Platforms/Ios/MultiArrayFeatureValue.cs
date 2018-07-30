53f9a5ca8e86eb12e762bcee5e1722a3c3b70909
MaikuB <25263378+MaikuB@users.noreply.github.com>
2018-07-30 11:47:01 +1000

    ios additions to predict output values

---
+++ src/BuildIt.ML/BuildIt.ML/Platforms/Ios/MultiArrayFeatureValue.cs
@@ -0,0 +1,9 @@
+﻿using CoreML;
+
+namespace BuildIt.ML.Platforms.Ios
+{
+    public class MultiArrayInputFeature : FeatureValue<MLMultiArray>
+    {
+        public override MLFeatureType Type => MLFeatureType.MultiArray;
+    }
+}
---
+++ src/BuildIt.ML/BuildIt.ML/Platforms/Ios/StringFeatureValue.cs
@@ -0,0 +1,9 @@
+﻿using CoreML;
+
+namespace BuildIt.ML.Platforms.Ios
+{
+    public class StringFeatureValue : FeatureValue<string>
+    {
+        public override MLFeatureType Type => MLFeatureType.String;
+    }
+}
