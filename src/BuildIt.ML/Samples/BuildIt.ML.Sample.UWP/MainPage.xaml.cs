a03c331f5e37b3bc45e504a1bef248a92fd56d0e
MaikuB <25263378+MaikuB@users.noreply.github.com>
2018-07-30 12:57:53 +1000

    add blank app as a start for the sample project

---
+++ src/BuildIt.ML/Samples/BuildIt.ML.Sample.UWP/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BuildIt.ML.Sample.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadApplication(new UI.App());
+        }
+    }
+}
