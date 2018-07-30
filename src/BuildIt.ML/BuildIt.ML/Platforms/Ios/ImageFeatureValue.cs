cb1478a750008e0abc2101345840100547d85d71
MaikuB <25263378+MaikuB@users.noreply.github.com>
2018-07-30 11:47:01 +1000

    ios additions to predict output values

---
+++ src/BuildIt.ML/BuildIt.ML/Platforms/Ios/ImageFeatureValue.cs
@@ -0,0 +1,10 @@
+﻿using CoreML;
+using CoreVideo;
+
+namespace BuildIt.ML.Platforms.Ios
+{
+    public class ImageFeatureValue : FeatureValue<CVPixelBuffer>
+    {
+        public override MLFeatureType Type => MLFeatureType.Image;
+    }
+}
---
+++ src/BuildIt.ML/BuildIt.ML/Platforms/Ios/LongFeatureValue.cs
@@ -0,0 +1,9 @@
+﻿using CoreML;
+
+namespace BuildIt.ML.Platforms.Ios
+{
+    public class LongFeatureValue : FeatureValue<long>
+    {
+        public override MLFeatureType Type => MLFeatureType.Int64;
+    }
+}
