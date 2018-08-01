54850fed5d0c88ac4591b1c7a5667af7cc7720e8
MaikuB <25263378+MaikuB@users.noreply.github.com>
2018-08-01 17:50:38 +1000

    start on creating wrapper passing frames

---
+++ src/BuildIt.Forms/BuildIt.Forms.Controls/MediaFrame.cs
@@ -0,0 +1,12 @@
+﻿namespace BuildIt.Forms.Controls
+{
+    public class MediaFrame
+    {
+        public MediaFrame(object nativeFrame)
+        {
+            NativeFrame = nativeFrame;
+        }
+
+        public object NativeFrame { get; }
+    }
+}
