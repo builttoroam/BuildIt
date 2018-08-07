7f98951fcaf648405e1b0753094b0450983d3db2
MaikuB <25263378+MaikuB@users.noreply.github.com>
2018-08-07 11:17:01 +1000

    cleaning up code further

--- src/BuildIt.Forms/BuildIt.Forms.Controls/Platforms/Ios/CameraPreviewControlRenderer.cs
+++ src/BuildIt.Forms/BuildIt.Forms.Controls/Platforms/Ios/CameraPreviewControlRenderer.cs
@@ -367,25 +367,4 @@ namespace BuildIt.Forms.Controls.Platforms.Ios
             return cameras;
         }
     }
-
-    internal class FrameExtractor : AVCaptureVideoDataOutputSampleBufferDelegate
-    {
-        private readonly Func<MediaFrame, Task> frameArrivedAction;
-
-        public FrameExtractor(Func<MediaFrame, Task> frameArrivedAction)
-        {
-            this.frameArrivedAction = frameArrivedAction;
-        }
-
-        public override async void DidOutputSampleBuffer(AVCaptureOutput captureOutput, CMSampleBuffer sampleBuffer, AVCaptureConnection connection)
-        {
-            using (sampleBuffer)
-            {
-                using (var pixelBuffer = sampleBuffer.GetImageBuffer() as CVPixelBuffer)
-                {
-                    await frameArrivedAction(new MediaFrame(pixelBuffer));
-                }
-            }
-        }
-    }
 }
--- src/BuildIt.ML/BuildIt.ML/Platforms/Android/CustomVisionClassifier.cs
+++ src/BuildIt.ML/BuildIt.ML/Platforms/Android/CustomVisionClassifier.cs
@@ -12,15 +12,15 @@ namespace BuildIt.ML
     {
         private const string OutputName = "loss";
         private const string InputName = "Placeholder";
+        private const string DataNormLayerPrefix = "data_bn";
         private TensorFlowInferenceInterface inferenceInterface;
         private string[] labels;
-        private string DataNormLayerPrefix = "data_bn";
         private bool hasNormalizationLayer;
 
         public async Task InitAsync(string modelName, string[] labels)
         {
             this.labels = labels;
-            var assets = global::Android.App.Application.Context.Assets;
+            var assets = Android.App.Application.Context.Assets;
 
             inferenceInterface = new TensorFlowInferenceInterface(assets, modelName + ".pb");
 
@@ -33,7 +33,6 @@ namespace BuildIt.ML
                     hasNormalizationLayer = true;
                 }
             }
-            System.Diagnostics.Debug.WriteLine("initialized");
         }
 
         public async Task<IReadOnlyList<ImageClassification>> ClassifyAsync(Stream imageStream)
@@ -59,16 +58,11 @@ namespace BuildIt.ML
 
         public async Task<IReadOnlyList<ImageClassification>> ClassifyNativeFrameAsync(object obj)
         {
-            System.Diagnostics.Debug.WriteLine("cast bytes");
             var bytes = obj as byte[];
-            System.Diagnostics.Debug.WriteLine($"decode {bytes.Length}");
-
             using (var bitmap = await BitmapFactory.DecodeByteArrayAsync(bytes, 0, bytes.Length))
             {
                 var outputs = new float[labels.Length];
-                System.Diagnostics.Debug.WriteLine("get pixels");
                 var floatValues = GetBitmapPixels(bitmap, 227, 227, hasNormalizationLayer);
-                System.Diagnostics.Debug.WriteLine("train");
                 inferenceInterface.Feed(InputName, floatValues.ToArray(), 1, 227, 227, 3);
                 inferenceInterface.Run(new string[] { OutputName });
                 inferenceInterface.Fetch(OutputName, outputs);
@@ -82,64 +76,12 @@ namespace BuildIt.ML
             }
         }
 
-        //private float[] GetBitmapPixels(Bitmap bitmap, int width, int height)
-        //{
-        //    var floatValues = new float[width * height * 3];
-        //    var imageMeanB = hasNormalizationLayer ? 0f : 124f;
-        //    var imageMeanG = hasNormalizationLayer ? 0f : 117f;
-        //    var imageMeanR = hasNormalizationLayer ? 0f : 105f;
-        //    System.Diagnostics.Debug.WriteLine($"has normalization layer {hasNormalizationLayer}");
-        //    using (var scaledBitmap = Bitmap.CreateScaledBitmap(bitmap, width, height, false))
-        //    {
-        //        using (var resizedBitmap = scaledBitmap.Copy(Bitmap.Config.Argb8888, false))
-        //        {
-        //            var intValues = new int[width * height];
-        //            resizedBitmap.GetPixels(intValues, 0, resizedBitmap.Width, 0, 0, resizedBitmap.Width, resizedBitmap.Height);
-
-        //            for (int i = 0; i < intValues.Length; ++i)
-        //            {
-        //                var val = intValues[i];
-
-        //                floatValues[i * 3 + 0] = ((val & 0xFF) - imageMeanB);
-        //                floatValues[i * 3 + 1] = (((val >> 8) & 0xFF) - imageMeanG);
-        //                floatValues[i * 3 + 2] = (((val >> 16) & 0xFF) - imageMeanR);
-        //            }
-
-        //            resizedBitmap.Recycle();
-        //        }
-
-        //        scaledBitmap.Recycle();
-        //    }
-
-        //    return floatValues;
-        //}
-
-        private static float ImageMeanR()
-        {
-  
-                    return 123.0f;
-        }
-
-        private static float ImageMeanG()
-        {
-
-                    return 117.0f;
- 
-        }
-
-        private static float ImageMeanB()
-        {
-
-                    return 104.0f;
-
-        }
-
-        public static float[] GetBitmapPixels(Bitmap bitmap, int width, int height, bool hasNormalizationLayer)
+        private static float[] GetBitmapPixels(Bitmap bitmap, int width, int height, bool hasNormalizationLayer)
         {
             var floatValues = new float[width * height * 3];
-            var imageMeanB = hasNormalizationLayer ? 0f : ImageMeanB();
-            var imageMeanG = hasNormalizationLayer ? 0f : ImageMeanG();
-            var imageMeanR = hasNormalizationLayer ? 0f : ImageMeanR();
+            var imageMeanR = hasNormalizationLayer ? 0f : 123f;
+            var imageMeanG = hasNormalizationLayer ? 0f : 117f;
+            var imageMeanB = hasNormalizationLayer ? 0f : 104f;
 
             using (var scaledBitmap = Bitmap.CreateScaledBitmap(bitmap, width, height, false))
             {
