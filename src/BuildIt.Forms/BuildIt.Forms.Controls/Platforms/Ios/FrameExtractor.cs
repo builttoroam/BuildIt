27455a7f455eaa040c10566696f2ad320fa81e55
MaikuB <25263378+MaikuB@users.noreply.github.com>
2018-08-07 11:17:01 +1000

    cleaning up code further

---
+++ src/BuildIt.Forms/BuildIt.Forms.Controls/Platforms/Ios/FrameExtractor.cs
@@ -0,0 +1,33 @@
+﻿using AVFoundation;
+using BuildIt.Forms.Controls;
+using BuildIt.Forms.Controls.Platforms.Ios;
+using CoreMedia;
+using CoreVideo;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BuildIt.Forms.Controls.Platforms.Ios
+{
+    internal class FrameExtractor : AVCaptureVideoDataOutputSampleBufferDelegate
+    {
+        private readonly Func<MediaFrame, Task> frameArrivedAction;
+
+        public FrameExtractor(Func<MediaFrame, Task> frameArrivedAction)
+        {
+            this.frameArrivedAction = frameArrivedAction;
+        }
+
+        public override async void DidOutputSampleBuffer(AVCaptureOutput captureOutput, CMSampleBuffer sampleBuffer, AVCaptureConnection connection)
+        {
+            using (sampleBuffer)
+            {
+                using (var pixelBuffer = sampleBuffer.GetImageBuffer() as CVPixelBuffer)
+                {
+                    await frameArrivedAction(new MediaFrame(pixelBuffer));
+                }
+            }
+        }
+    }
+}
