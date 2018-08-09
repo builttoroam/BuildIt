def03e7508e46fc9e1f7e38672fa2870b6ff247b
MaikuB <25263378+MaikuB@users.noreply.github.com>
2018-08-09 14:21:03 +1000

    progress on changes for camera2

---
+++ src/BuildIt.Forms/BuildIt.Forms.Controls/Platforms/Android/CameraCaptureListener.cs
@@ -0,0 +1,84 @@
+﻿using Android.Hardware.Camera2;
+using Java.IO;
+using Java.Lang;
+
+namespace BuildIt.Forms.Controls.Platforms.Android
+{
+    public class CameraCaptureListener : CameraCaptureSession.CaptureCallback
+    {
+        private readonly CameraPreviewControlRenderer owner;
+
+        public CameraCaptureListener(CameraPreviewControlRenderer owner)
+        {
+            if (owner == null)
+                throw new System.ArgumentNullException("owner");
+            this.owner = owner;
+        }
+
+        public override void OnCaptureCompleted(CameraCaptureSession session, CaptureRequest request, TotalCaptureResult result)
+        {
+            Process(result);
+        }
+
+        public override void OnCaptureProgressed(CameraCaptureSession session, CaptureRequest request, CaptureResult partialResult)
+        {
+            Process(partialResult);
+        }
+
+        private void Process(CaptureResult result)
+        {
+            switch (owner.mState)
+            {
+                case CameraPreviewControlRenderer.STATE_WAITING_LOCK:
+                    {
+                        Integer afState = (Integer)result.Get(CaptureResult.ControlAfState);
+                        if (afState == null)
+                        {
+                            owner.CaptureStillPicture();
+                        }
+
+                        else if ((((int)ControlAFState.FocusedLocked) == afState.IntValue()) ||
+                                   (((int)ControlAFState.NotFocusedLocked) == afState.IntValue()))
+                        {
+                            // ControlAeState can be null on some devices
+                            Integer aeState = (Integer)result.Get(CaptureResult.ControlAeState);
+                            if (aeState == null ||
+                                    aeState.IntValue() == ((int)ControlAEState.Converged))
+                            {
+                                owner.mState = CameraPreviewControlRenderer.STATE_PICTURE_TAKEN;
+                                owner.CaptureStillPicture();
+                            }
+                            else
+                            {
+                                owner.RunPrecaptureSequence();
+                            }
+                        }
+                        break;
+                    }
+                case CameraPreviewControlRenderer.STATE_WAITING_PRECAPTURE:
+                    {
+                        // ControlAeState can be null on some devices
+                        Integer aeState = (Integer)result.Get(CaptureResult.ControlAeState);
+                        if (aeState == null ||
+                                aeState.IntValue() == ((int)ControlAEState.Precapture) ||
+                                aeState.IntValue() == ((int)ControlAEState.FlashRequired))
+                        {
+                            owner.mState = CameraPreviewControlRenderer.STATE_WAITING_NON_PRECAPTURE;
+                        }
+                        break;
+                    }
+                case CameraPreviewControlRenderer.STATE_WAITING_NON_PRECAPTURE:
+                    {
+                        // ControlAeState can be null on some devices
+                        Integer aeState = (Integer)result.Get(CaptureResult.ControlAeState);
+                        if (aeState == null || aeState.IntValue() != ((int)ControlAEState.Precapture))
+                        {
+                            owner.mState = CameraPreviewControlRenderer.STATE_PICTURE_TAKEN;
+                            owner.CaptureStillPicture();
+                        }
+                        break;
+                    }
+            }
+        }
+    }
+}
---
+++ src/BuildIt.Forms/BuildIt.Forms.Controls/Platforms/Android/CameraCaptureStillPictureSessionCallback.cs
@@ -0,0 +1,28 @@
+﻿using Android.Hardware.Camera2;
+using Android.Util;
+
+namespace BuildIt.Forms.Controls.Platforms.Android
+{
+    public class CameraCaptureStillPictureSessionCallback : CameraCaptureSession.CaptureCallback
+    {
+        private static readonly string TAG = "CameraCaptureStillPictureSessionCallback";
+
+        private readonly CameraPreviewControlRenderer owner;
+
+        public CameraCaptureStillPictureSessionCallback(CameraPreviewControlRenderer owner)
+        {
+            if (owner == null)
+                throw new System.ArgumentNullException("owner");
+            this.owner = owner;
+        }
+
+        public override void OnCaptureCompleted(CameraCaptureSession session, CaptureRequest request, TotalCaptureResult result)
+        {
+            //// If something goes wrong with the save (or the handler isn't even 
+            //// registered, this code will toast a success message regardless...)
+            //owner.ShowToast("Saved: " + owner.mFile);
+            //Log.Debug(TAG, owner.mFile.ToString());
+            //owner.UnlockFocus();
+        }
+    }
+}
