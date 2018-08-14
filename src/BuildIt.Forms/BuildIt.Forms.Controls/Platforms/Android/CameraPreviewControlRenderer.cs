fb00d055d018919d4159b0705f86b938d16e3c73
MaikuB <25263378+MaikuB@users.noreply.github.com>
2018-08-14 17:35:34 +1000

    update xamarin forms and clean up spacing from casting in previous commit

--- src/BuildIt.Forms/BuildIt.Forms.Controls/Platforms/Android/CameraPreviewControlRenderer.cs
+++ src/BuildIt.Forms/BuildIt.Forms.Controls/Platforms/Android/CameraPreviewControlRenderer.cs
@@ -619,7 +619,7 @@ namespace BuildIt.Forms.Controls.Platforms.Android
             }
 
             var capabilities = cameraCharacteristics.Get(CameraCharacteristics.RequestAvailableCapabilities).ToArray<int>();
-            if (capabilities.Any(c => c == (int) RequestAvailableCapabilities.ManualSensor))
+            if (capabilities.Any(c => c == (int)RequestAvailableCapabilities.ManualSensor))
             {
                 supportedFocusModes.Add(CameraFocusMode.Manual);
             }
