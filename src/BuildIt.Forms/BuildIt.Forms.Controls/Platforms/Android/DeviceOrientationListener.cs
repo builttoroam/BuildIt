9b47f5b216d862f63be8ef30b769cd4cf5bceeaf
MaikuB <25263378+MaikuB@users.noreply.github.com>
2018-08-23 16:27:42 +1000

    add device orientation listener that will be used for responding to orientation changes so camera preview will be adjusted accordingly

---
+++ src/BuildIt.Forms/BuildIt.Forms.Controls/Platforms/Android/DeviceOrientationListener.cs
@@ -0,0 +1,26 @@
+﻿using Android.Content;
+using Android.Views;
+using System;
+
+namespace BuildIt.Forms.Controls.Platforms.Android
+{
+    public class DeviceOrientationListener : OrientationEventListener
+    {
+        private readonly Action orientationChangedAction;
+        private int previousOriention = 0;
+
+        public DeviceOrientationListener(Context context, Action orientationChangedAction)
+            : base(context)
+        {
+            this.orientationChangedAction = orientationChangedAction;
+        }
+
+        public override void OnOrientationChanged(int orientation)
+        {
+            if (Math.Abs(orientation - previousOriention) >= 90)
+            {
+                orientationChangedAction?.Invoke();
+            }
+        }
+    }
+}
