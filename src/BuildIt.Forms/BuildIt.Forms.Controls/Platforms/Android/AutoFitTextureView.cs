6d2f344308e14d2d07cfa51eaaba32e569d62ee8
MaikuB <25263378+MaikuB@users.noreply.github.com>
2018-08-24 11:52:23 +1000

    cast values in autofittextureview to float for consistency

--- src/BuildIt.Forms/BuildIt.Forms.Controls/Platforms/Android/AutoFitTextureView.cs
+++ src/BuildIt.Forms/BuildIt.Forms.Controls/Platforms/Android/AutoFitTextureView.cs
@@ -66,7 +66,7 @@ namespace BuildIt.Forms.Controls.Platforms.Android
                     break;
 
                 case Aspect.AspectFill:
-                    var previewAspectRatio = (double)ratioWidth / ratioHeight;
+                    var previewAspectRatio = (float)ratioWidth / ratioHeight;
                     if (previewAspectRatio > 1)
                     {
                         // width > height, so keep the height but scale the width to meet aspect ratio
