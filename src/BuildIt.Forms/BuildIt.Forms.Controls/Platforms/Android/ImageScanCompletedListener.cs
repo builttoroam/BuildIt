805f1d3af88b9f4ef6c7c555bc634e53bae6a0bb
mkieres <mikolaj@builttoroam.com>
2018-10-31 10:19:51 +1100

    [Droid] Fixing capturing and saving a photo on android

---
+++ src/BuildIt.Forms/BuildIt.Forms.Controls/Platforms/Android/ImageScanCompletedListener.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+using Android.Media;
+using Android.Net;
+
+namespace BuildIt.Forms.Controls.Platforms.Android
+{
+    public class ImageScanCompletedListener : Java.Lang.Object, MediaScannerConnection.IOnScanCompletedListener
+    {
+        public void OnScanCompleted(string path, Uri uri)
+        {
+            Debug.WriteLine($"[{nameof(ImageScanCompletedListener)}] Image scan completed. The scanned image path is {path}");
+        }
+    }
+}
