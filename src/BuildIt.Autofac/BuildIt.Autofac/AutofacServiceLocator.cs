a97b66ebc0aaeb4114158603ffacb464b7e9326f
Nick Randolph <nick@builttoroam.com>
2018-12-30 10:17:49 +1100

    Updating autofac nuget refs

--- src/BuildIt.Autofac/BuildIt.Autofac/AutofacServiceLocator.cs
+++ src/BuildIt.Autofac/BuildIt.Autofac/AutofacServiceLocator.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using Autofac;
+using CommonServiceLocator;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Autofac;
-using BuildIt.ServiceLocation;
 
 namespace BuildIt.Autofac
 {
