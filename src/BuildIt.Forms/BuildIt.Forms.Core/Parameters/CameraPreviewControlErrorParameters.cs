c588304218141c162150000d79e97522de1762a9
Nick Randolph <nick@builttoroam.com>
2018-12-31 13:07:59 +1100

    Updating nuget packages Fixing visual state reference

--- src/BuildIt.Forms/BuildIt.Forms.Core/Parameters/CameraPreviewControlErrorParameters.cs
+++ src/BuildIt.Forms/BuildIt.Forms.Core/Parameters/CameraPreviewControlErrorParameters.cs
@@ -4,29 +4,23 @@
     /// Provides information about the camera preview error
     /// </summary>
     /// <typeparam name="TData">Data object passed with the error</typeparam>
-    public class CameraPreviewControlErrorParameters<TData>
+    public class CameraPreviewControlErrorParameters<TData> : CameraPreviewControlErrorParameters
     {
-        public CameraPreviewControlErrorParameters(string[] errors, TData data, bool handled = false)
-        {
-            Errors = errors;
-            Data = data;
-            Handled = Handled;
-        }
-
         /// <summary>
-        /// Gets error messages
+        /// Initializes a new instance of the <see cref="CameraPreviewControlErrorParameters"/> class.
         /// </summary>
-        public string[] Errors { get; }
+        /// <param name="errors">errors to emit</param>
+        /// <param name="data">the data to include relating to the error</param>
+        /// <param name="handled">indicates whether the error has been handled or not</param>
+        public CameraPreviewControlErrorParameters(string[] errors, TData data, bool handled = false) : base(errors, handled)
+        {
+            Data = data;
+        }
 
         /// <summary>
         /// Gets error data
         /// </summary>
         public TData Data { get; }
-
-        /// <summary>
-        /// Gets a value indicating whether an error was handled
-        /// </summary>
-        public bool Handled { get; }
     }
 
     /// <summary>
@@ -34,6 +28,11 @@
     /// </summary>
     public class CameraPreviewControlErrorParameters
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CameraPreviewControlErrorParameters"/> class.
+        /// </summary>
+        /// <param name="errors">errors to emit</param>
+        /// <param name="handled">indicates whether the error has been handled or not</param>
         public CameraPreviewControlErrorParameters(string[] errors, bool handled = false)
         {
             Errors = errors;
