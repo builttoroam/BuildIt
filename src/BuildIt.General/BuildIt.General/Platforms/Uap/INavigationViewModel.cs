293906a7136ae206e218279e03e68d0ed6f67fc7
Nick Randolph <nick@builttoroam.com>
2019-09-21 23:21:54 +1000

    Adding navigation interface and base page

---
+++ src/BuildIt.General/BuildIt.General/Platforms/Uap/INavigationViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace BuildIt.UI
+{
+    public interface INavigationViewModel
+    {
+        void OnAppearing(object parameter = null);
+
+        void OnLeaving();
+    }
+
+    public interface IApplicationWithMapping
+    {
+        IDictionary<Type, IEventMap[]> Maps { get; }
+    }
+}
---
+++ src/BuildIt.General/BuildIt.General/Platforms/Uap/MappingBasePage.cs
@@ -0,0 +1,45 @@
+﻿using System.Globalization;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using DoUnoMvvm.Core.ViewModels;
+using Windows.UI.Xaml;
+
+namespace BuildIt.UI
+{
+    public partial class BasePage : Page
+    {
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            var vm = this.DataContext;
+            var mappings = (Application.Current as IApplicationWithMapping).Maps;
+            if (vm != null && mappings.TryGetValue(vm.GetType(), out var maps))
+            {
+                foreach (var map in maps)
+                {
+                    map.Wire(vm);
+                }
+            }
+
+            (vm as INavigationViewModel)?.OnAppearing(e.Parameter);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+
+            var vm = this.DataContext;
+            var mappings = (Application.Current as IApplicationWithMapping).Maps;
+            if (vm != null && mappings.TryGetValue(vm.GetType(), out var maps))
+            {
+                foreach (var map in maps)
+                {
+                    map.Unwire(vm);
+                }
+            }
+
+            (vm as INavigationViewModel)?.OnLeaving();
+        }
+    }
+}
